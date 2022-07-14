b5a390187507658e75adac04e07852584d73a4c0
emmaiwasaki <ernyy.ni@gmail.com>
2022-07-13 23:07:09 -0700

    added basics

---
+++ basics/exercises.js
@@ -0,0 +1,43 @@
+'emma' + ' ' + 'iwasaki'; // #1
+
+let number = 4936;
+let six = number % 10;
+number = (number - six) / 10;
+let three = number % 10;
+number = (number - three) / 10;
+let nine = number % 10
+let four = (number - nine) / 10 // #2
+
+/* 
+string 
+boolean
+float
+number
+undefined
+object
+*/  // #3
+
+// the first param is a string #4 
+
+// console.log(Number('5') + 10); #5
+
+// console.log(`The value of 5 + 10 is ${Number('5') + 10}`); #6
+
+// will result in undefined #7
+
+let names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin']; // #8
+
+let pets = {
+  asta = 'dog',
+  butterscotch = 'cat',
+  pudding = 'cat',
+  neptune = 'fish',
+  darwin = 'lizard',
+}; // #9
+
+// false #10
+
+// 3 #11
+
+// true #12
+
