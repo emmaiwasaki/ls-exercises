ea0edc66836d0e473b1eedc4c7548db99aa35e6c
emmaiwasaki <ernyy.ni@gmail.com>
2022-07-13 23:14:08 -0700

    added preparations exercises

--- preparations/exercises.js
+++ preparations/exercises.js
@@ -1,1 +1,15 @@
-hi
+/*
+substring > string > prototype 
+create > object > static
+fromCharCode > ostring > static
+slice > array, string > prototype
+toString > string, object, array, number > prototype
+*/ // #5
+
+// index, lazyDog, operand2 #6
+
+// index, lazyDog, operand2, CatName #7
+
+// BIG_NUMBER #8
+
+// snake_case, quick_Fox #9s
