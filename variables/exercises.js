b23727f606c24f0c4085d5ed8e63bcff552ad0fc
emmaiwasaki <ernyy.ni@gmail.com>
2022-07-15 17:44:58 -0700

    added new exercises

---
+++ variables/exercises.js
@@ -0,0 +1,26 @@
+// let insertName = 'Victor';
+// console.log(`Good Morning, ${insertName}`);
+// console.log(`Good Afternoon, ${insertName}`);
+// console.log(`Good Evening, ${insertName}`); 
+// #1
+
+// let age = 20;
+// console.log(`In 10 years, you will be ${age + 10} years old `);
+// console.log(`In 20 years, you will be ${age + 20} years old `);
+// console.log(`In 30 years, you will be ${age + 30} years old `);
+// console.log(`In 40 years, you will be ${age + 40} years old `);
+// #2
+
+// error #3
+
+// prints Victor greeting three times 
+const NAME = 'Victor';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
+NAME = 'Joe';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
