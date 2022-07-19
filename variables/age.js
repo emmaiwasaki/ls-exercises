05e489224a29ea91ec107156f3560e9ce58f88a6
emmaiwasaki <ernyy.ni@gmail.com>
2022-07-18 20:50:58 -0700

    added age.js

---
+++ variables/age.js
@@ -0,0 +1,6 @@
+let rlSync = require('readline-sync');
+let age = rlSync.question("What's your first age?\n");
+console.log(`In 10 years, you will be ${Number(age) + 10} years old `);
+console.log(`In 20 years, you will be ${Number(age) + 20} years old `);
+console.log(`In 30 years, you will be ${Number(age) + 30} years old `);
+console.log(`In 40 years, you will be ${Number(age) + 40} years old `);
