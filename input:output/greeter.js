ed10623bbd916a7699abb302074bb2f676e09997
emmaiwasaki <ernyy.ni@gmail.com>
2022-07-27 12:48:02 -0700

    added new function

--- input:output/greeter.js
+++ input:output/greeter.js
@@ -1,3 +1,14 @@
 let rlSync = require('readline-sync');
-let name = rlSync.question("What's your name?\n");
-console.log(`Hello, ${name}!`);
+let firstName = rlSync.question("What's your first name?\n");
+let lastName = rlSync.question("What's your last name?\n");
+console.log(`Hello, ${firstName} ${lastName}!`);
+
+let getName = (prompt) => {
+  let rlSync2 = require('readline-sync');
+  let name = rlSync2.question(prompt);
+  return name;
+} 
+
+let firstName2 = getName('what is your first name?');
+let lastName2 = getName('what is your last name?');
+console.log(`Hello, ${firstName2} ${lastName2}!`);
